0f6d794fb173a24fdb50d99f7b2a633dcadb1e84
douglas1001 <douglassilva1001@gmail.com>
2025-08-12 23:25:03 -0300

    resultado

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-let quantidadeVitorias = 0;
-let quantidadeDerrotas = 0;
+let quantidadeVitorias = 100;
+let quantidadeDerrotas = 50;
 
+//Realizando o calculo do saldo de vitorias
 function calculoVitoria (quantidadeVitorias, quantidadeDerrotas){
-    saldo = 
-
+   let resultado = quantidadeVitorias - quantidadeDerrotas
+    return resultado
 }
+
+//Atribuindo o resultado do calculo feito na função calculoVitoria a uma variável
+let saldoFinal = calculoVitoria (quantidadeVitorias, quantidadeDerrotas);
+let nivel = ""
+
+//Categorizando o Nivel do heroi
+switch (true) {
+    case (saldoFinal <= 10):
+        nivel = "Ferro"
+        break;
+    case (saldoFinal <= 20):
+        nivel = "Bronze"
+        break;
+    case (saldoFinal <= 50):
+        nivel = "Prata"
+        break;
+    case (saldoFinal <= 80):
+        nivel = "Ouro"
+        break;
+    case (saldoFinal <= 90):
+        nivel = "Diamante"
+        break;
+    case (saldoFinal <= 100):
+        nivel = "Lendário"
+        break;
+    default:
+        nivel = "Imortal"
+}
+
+console.log("O Herói tem de saldo de " + saldoFinal + " está no nível de " + nivel );
